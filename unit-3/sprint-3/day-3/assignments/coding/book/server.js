ca36994ce30d5f51f81d85915cb564e0056dfd5e
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-09-25 14:05:27 +0530

    book.json

--- unit-3/sprint-3/day-3/assignments/coding/book/server.js
+++ unit-3/sprint-3/day-3/assignments/coding/book/server.js
@@ -8,13 +8,20 @@ app.use(express.json());
 
 app.use(adding);
 
+
+// listenong to PORT 
+
 app.listen(2345, (req, res) => {
   console.log("listining on 2345");
 });
 
+// getting all book Details
+
 app.get("/", (req, res) => {
   res.send(users);
 });
+
+// post the book details
 
 app.post("/books", (req, res) => {
   res.send({
@@ -22,6 +29,37 @@ app.post("/books", (req, res) => {
     age: 21,
   });
 });
+
+//get the book by ids
+
+app.get("/books/:id", (req, res) => {
+  let id = req.params.id;
+   let book = users.filter((el) => {
+    return el.id == id;
+  });
+  res.send(book);
+});
+// patch or update the book Details
+app.patch("/books/:id", (req, res) => {
+  let id = req.params.id;
+   let book = users.filter((el) => {
+    return el.id == id;
+  });
+  res.send(req.body);
+});
+
+
+// delete book
+
+app.delete("/books/:id", (req, res) => {
+  let id = req.params.id;
+   let book = users.filter((el) => {
+    return el.id == id;
+  });
+  res.send(req.body==[]);
+});
+
+
 
 function adding(req, res, next) {
   console.log("adding before");
