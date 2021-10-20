2ac69ec15f00a1a8f73bc68cf2d95e75b878d22d
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-10-20 16:42:12 +0530

    count

--- unit-4/React/react2/we/reactweday2/src/App.js
+++ unit-4/React/react2/we/reactweday2/src/App.js
@@ -1,10 +1,28 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  // let count = 1;
   return (
     <div className="App">
-      <h1>Abhishek</h1>
+      <h1>Couter</h1>
+      <h3>{count}</h3>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Add
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Remove
+      </button>
     </div>
   );
 }
