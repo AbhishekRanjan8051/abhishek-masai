6b058bcc44f3ac467ad1ac04b259deb16a45d74f
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-10-22 14:16:33 +0530

    form data

--- unit-4/React/react3/preclass/list-keys/src/components/Form.jsx
+++ unit-4/React/react3/preclass/list-keys/src/components/Form.jsx
@@ -1,12 +1,55 @@
 import React from "react";
 
+const initstate = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 function Form() {
+  const [state, setState] = React.useState(initstate);
+
+  const { name, email, password } = state;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(state, "data is");
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="name"></input>
-        <input type="text" placeholder="email"></input>
-        <input type="text" placeholder="password"></input>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        ></input>
+        <br />
+        <input type="submit" value="SUMBIT DATA" />
       </form>
     </div>
   );
