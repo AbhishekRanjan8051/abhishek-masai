4d90c792bbaaf0295fbd176349549912933eed42
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-10-26 22:09:02 +0530

    form user final

--- unit-4/React/react6/preclass/eventsform/src/App.js
+++ unit-4/React/react6/preclass/eventsform/src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import React from "react";
 import { FormTag } from "./components/FormTag";
-// import { Form } from "./components/Form";
-// import { Condition } from "./components/Condition";
-// import { UseRef } from "./components/UseRef";
-// import { UserefImage } from "./components/UserefImage";
+import { Form } from "./components/Form";
+import { Condition } from "./components/Condition";
+import { UseRef } from "./components/UseRef";
+import { UserefImage } from "./components/UserefImage";
 
 function App() {
   return (
     <div className="App">
       <h1>Abhishek Ranjan</h1>
-      {/* <Form></Form> */}
-      {/* <Condition></Condition> */}
-      {/* <UseRef></UseRef> */}
-      {/* <UserefImage></UserefImage> */}
+      <Form></Form>
+      <Condition></Condition>
+      <UseRef></UseRef>
+      <UserefImage></UserefImage>
       <FormTag></FormTag>
     </div>
   );
