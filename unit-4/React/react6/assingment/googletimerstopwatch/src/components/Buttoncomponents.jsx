9043958c0750e20b25313b32abc0e5678f3c3808
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-10-28 17:21:14 +0530

    stopwatch

---
+++ unit-4/React/react6/assingment/googletimerstopwatch/src/components/Buttoncomponents.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./css/button.css";
+
+function Buttoncomponents(props) {
+  return (
+    <div className="Button">
+      {props.status === 0 ? (
+        <button className="btn" onClick={props.start}>
+          Start
+        </button>
+      ) : (
+        ""
+      )}
+
+      {props.status === 1 ? (
+        <div>
+          <button className="btn3" onClick={props.stop}>
+            Stop
+          </button>
+          <button className="btn4" onClick={props.reset}>
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      {props.status === 2 ? (
+        <div>
+          <button className="btn5" onClick={props.resume}>
+            Resume
+          </button>
+          <button className="btn4" onClick={props.reset}>
+            Reset
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+export { Buttoncomponents };
