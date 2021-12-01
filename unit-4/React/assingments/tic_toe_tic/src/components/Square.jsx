29d38d55c526f795a37616752da2c13d4c6db1f5
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-12-01 18:23:54 +0530

    done tic toe tic game app

--- unit-4/React/assingments/tic_toe_tic/src/components/Square.jsx
+++ unit-4/React/assingments/tic_toe_tic/src/components/Square.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
-function Square(){
-
-    return (
-        <div>X</div>
-    )
+const Box = styled.div`
+  border: 5px solid lightblue;
+  min-width: 100px;
+  min-height: 100px;
+  font-size: 64px;
+  text-align: center;
+  color: #1cdbf5;
+  font-weight: bolder;
+  padding: 30px;
+  
+`;
+function Square(props) {
+  return (
+    <div>
+      <Box onClick={props.onClick}>{props.state}</Box>
+    </div>
+  );
 }
-export {Square}
+export { Square };
