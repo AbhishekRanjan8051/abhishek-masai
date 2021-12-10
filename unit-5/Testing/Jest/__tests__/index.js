3f60c2047494b6e06022694609fa01ab2386e565
AbhishekRanjan8051 <abhishekranjan8051@gmail.com>
2021-12-10 07:53:49 +0530

    jest

--- unit-5/Testing/Jest/__tests__/index.js
+++ unit-5/Testing/Jest/__tests__/index.js
@@ -2,7 +2,7 @@ const sum = require("./index");
 
 // console.log(sum(5, 3));
 
-describe("sum function is defined", ()=> {
+describe("sum function is defined", () => {
   test("sum is def", () => {
     expect(sum).toBeDefined();
   });
@@ -26,7 +26,22 @@ describe("equal", () => {
     expect([2]).toEqual([2]);
   });
   test("array a and b is equal []", () => {
-    let a = [1, 2]; let b = [1, 2];
+    let a = [1, 2];
+    let b = [1, 2];
     expect([a]).toEqual([b]);
+  });
+
+  // not Equal
+  test("array a and b is not equal []", () => {
+    let a = [1, 2];
+    let b = [1, 2, 3];
+    expect([a]).not.toEqual([b]);
+  });
+
+  // if array element present in array  b
+  test("array a and b is not equal []", () => {
+    let a = [1, 2, 3];
+    let b = [1, 2];
+    expect(a).toEqual(expect.arrayContaining(b));
   });
 });
